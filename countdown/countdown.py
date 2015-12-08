c2e42072dc3ae5cd833c4cd42d2a500968cd784f
= <jacobfionngoldberg@gmail.com>
2015-12-08 21:22:09 +0200

    displays countdown

--- countdown/countdown.py
+++ countdown/countdown.py
@@ -1,18 +1,13 @@
-#countdown.py
-#
-#plays a sound when countdown number reaches 0
+# countdown.py
+# plays a sound at the end of a countdown 
 
 import time, argparse, pyglet
 from fprint import fprint
 
-def display_countdown(delta, run_time):
-	#clock_line = '|' + ('#' * (run_time - delta)) + ('-' * delta) + '|' + '[' + str(delta) + ' mins]' 
-	#for i in range (60):
-	#	print clock_line	
+def display_countdown(delta, run_time):	
 	fprint ('.')
 
 def play_sound():
-
 	sound = pyglet.media.load('clock-tick2.wav')
 	sound.play()
 	
@@ -22,29 +17,16 @@ def play_sound():
 	pyglet.clock.schedule_once(exit, 2.6)
 	pyglet.app.run()
 
-def main():
-	
+def main():	
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--minutes', help = 'countdown from specified minutes', type = int)
 	args = parser.parse_args()
-	
-	run_time = args.minutes * 60
-
 	if args.minutes:
-		completion_time = time.time() + run_time
+		completion_time = time.time() + (args.minutes * 60) 
 	else:
-		completion_time = time.time() + 100
-
-	last_printed_digit = run_time
-	delta = 1
-
-	while (delta > 0):
-		
-		delta = int((completion_time - time.time()) / 60)
-		if (delta < last_printed_digit):
-			display_countdown(delta, run_time / 60)
-			last_printed_digit = delta
-
+		completion_time = time.time()
+	while time.time() <= completion_time:
+		print completion_time - time.time()
   	play_sound()
 	
 if (__name__=='__main__'):
