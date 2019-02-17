513fd2e6a2e686c3903eec42bc78f1bf7c55b8d8
Jacob <jacobfionngoldberg@gmail.com>
2019-02-17 22:39:14 +0700

    log your life ;)

--- lifelog.py
+++ lifelog.py
@@ -1,5 +1,5 @@
 """
-A way to keep track of what needs doing
+Log your life? No, just the little important things we tend to forget...
 """
 import sqlite3
 import sys
@@ -7,6 +7,7 @@ import time
 
 
 def save(entry):
+    """Persist a log entry"""
     conn = db_conn()
     sql = "insert into log (datetime, entry) values(strftime('%s','now'), '{}')"
     conn.execute(sql.format(entry))
@@ -14,6 +15,7 @@ def save(entry):
 
 
 def db_conn():
+    """Connect to a database and setup table if required"""
     conn = sqlite3.connect('lldb')
     conn.cursor().execute((
         'create table if not exists log'
@@ -25,6 +27,7 @@ def db_conn():
 
 
 def lifelog(conn):
+    """Extract our lifelog from the db into memory"""
     lifelog = {}
     cur = conn.execute('select * from log')
     for _, datetime, entry in cur.fetchall():
@@ -32,11 +35,11 @@ def lifelog(conn):
     return lifelog
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     if len(sys.argv) == 1:
         save(input())
     if len(sys.argv) > 1 and sys.argv[1] == 'list':
         lifelog = lifelog(db_conn())
         for datetime in sorted(lifelog.keys(), reverse=True):
-            print(datetime.tm_year, datetime.tm_mon,datetime.tm_mday, end=': ')
+            print(datetime.tm_year, datetime.tm_mon, datetime.tm_mday, end=': ')
             print(lifelog[datetime][:55], '...', sep='')
